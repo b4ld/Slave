f50db22b9efca0ff14b28a93d665dbf560dfeb39
Heroslender <bruno.martins1998@hotmail.com>
2019-03-22 21:58:49 +0000

    Update logger console format

--- daemon/src/config/logger.js
+++ daemon/src/config/logger.js
@@ -1,39 +1,62 @@
 const winston = require('winston');
-const format = winston.format; 
+const format = winston.format;
 require('winston-daily-rotate-file');
+const colors = require('colors/safe');
 
 const logger = winston.createLogger({
   level: 'info',
   format: format.combine(
     format.splat(),
     format.timestamp({ format: 'HH:mm:ss.SSS' }),
-    format.printf(info => `${info.timestamp} [${info.level}] ${info.message}${info.stack ? '\n' + info.stack : ''}`)
+    format.printf(
+      info =>
+        `${info.timestamp} [${info.level}] ${info.message}${
+          info.stack ? '\n' + info.stack : ''
+        }`
+    )
   ),
   transports: [
     new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.DailyRotateFile({ 
+    new winston.transports.DailyRotateFile({
       filename: '%DATE%.log',
       dirname: './logs',
       datePattern: 'YYYY-MM-DD',
       zippedArchive: true,
-      maxFiles: '14d'
-    })
-  ]
+      maxFiles: '14d',
+    }),
+  ],
 });
 
 if (process.env.NODE_ENV !== 'production') {
   // Log to console in development
-  logger.add(new winston.transports.Console({
-    format: format.combine(
-      format(info => ({ ...info, level: info.level.toUpperCase() }))(),
-      format.colorize(),
-      format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss.SSS' }),
-      format.printf(info => `${info.timestamp} [${info.level}]${info.label ? `[${info.label}]` : ''}: ${info.message}${info.stack ? `\n${info.stack}` : ''}`)
-    )
-  }));
+  logger.add(
+    new winston.transports.Console({
+      format: format.combine(
+        format(info => ({ ...info, level: info.level.toUpperCase() }))(),
+        format.colorize(),
+        winston.format((info, opts) => {
+          if (info.timestamp) {
+            info.timestamp = colors.green(info.timestamp);
+          }
+          if (info.label) {
+            info.label = colors.yellow(info.label);
+          }
+
+          return info;
+        })(),
+        format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss.SSS' }),
+        format.printf(
+          info =>
+            `${info.timestamp} ${info.level} ${
+              info.label ? `[${info.label}] ` : ''
+            }${info.message}${info.stack ? `\n${info.stack}` : ''}`
+        )
+      ),
+    })
+  );
 }
 
-module.exports = (opts) => {
+module.exports = opts => {
   if (typeof opts === 'string') {
     return logger.child({ label: opts });
   } else if (typeof opts === 'object') {
