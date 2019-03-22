f240de8cdc4035540510828542b339f32eb1a0ba
Heroslender <bruno.martins1998@hotmail.com>
2019-03-22 22:14:12 +0000

    Add configuration parser

---
+++ daemon/src/config/config.js
@@ -0,0 +1,209 @@
+const path = require('path');
+const fs = require('fs');
+const xml = require('xml2js');
+
+let logger = require('./logger')('Config');
+const ConfigurationException = require('./ConfigurationException');
+const ServerPropertyType = require('../server/ServerPropertyType');
+
+const configFilePath = path.join(__dirname, '../slave-config-template.xml');
+
+const parser = new xml.Parser({
+  explicitArray: false,
+  attrValueProcessors: [xml.processors.parseBooleans],
+});
+const configFile = fs.readFileSync(configFilePath);
+
+/** @type {Configuration} */
+const configuration = {};
+
+parser.parseString(configFile, (err, result) => {
+  if (err) {
+    logger.info(err);
+    return;
+  }
+  logger.info(`Loading ${path.basename(configFilePath)}...`);
+
+  if (!result.Configuration) {
+    logger.info('Invalid configuration file!');
+    return;
+  }
+
+  const config = result.Configuration;
+
+  if (
+    !config.Daemon ||
+    !config.Daemon.MountDir ||
+    !config.Daemon.PortRange ||
+    !config.Daemon.PortRange.Start ||
+    !config.Daemon.PortRange.End
+  ) {
+    logger.info('Invalid daemon configuration!');
+    return;
+  }
+
+  configuration.mountDir = config.Daemon.MountDir;
+  configuration.portRange = {
+    start: config.Daemon.PortRange.Start,
+    end: config.Daemon.PortRange.End,
+  };
+  configuration.servers = {};
+
+  if (config.Servers) {
+    if (Array.isArray(config.Servers.Server)) {
+      for (const server of config.Servers.Server) {
+        const s = parseServer(server);
+        configuration.servers[s.name] = s;
+      }
+    } else {
+      const s = parseServer(config.Servers.Server);
+      configuration.servers[s.name] = s;
+    }
+  }
+  logger.info(`Configuration loaded!`);
+});
+
+/**
+ *
+ * @param {object} serverObject
+ * @returns {ServerType} server
+ */
+function parseServer(serverObject) {
+  logger = logger.child({ subLabel: serverObject.Name });
+  assertFieldDefined(serverObject.Image, 'Server image is undefined!');
+
+  if (!serverObject.Properties) {
+    serverObject.Properties = { Property: [] };
+  }
+
+  /** @type {ServerType} */
+  const server = {
+    name: assertFieldDefined(serverObject.Name, 'Server name is undefined!'),
+    image: {
+      name: assertFieldDefined(
+        serverObject.Image.Name,
+        'Server image name is undefined!'
+      ),
+    },
+    properties: parseServerProperties(
+      serverObject.Name,
+      serverObject.Properties.Property
+    ),
+  };
+
+  logger.info(`Server configuration loaded for "${server.name}"!`);
+  return server;
+}
+
+/**
+ *
+ * @param {object[]} props
+ * @returns {ServerProperties}
+ */
+function parseServerProperties(serverName, props) {
+  /** @type {ServerProperties} */
+  const properties = {};
+
+  if (props) {
+    if (Array.isArray(props)) {
+      for (const prop of props) {
+        if (!prop.$ || !prop.$.name) {
+          continue;
+        }
+
+        const property = isValidServerProperty(prop.$.name);
+        if (!property) {
+          logger.warn(
+            `${serverName} - The property ${prop.$.name} does not exist!`
+          );
+          continue;
+        }
+
+        properties[property.name] = prop.$.type || property.default;
+      }
+    } else if (props.$ && props.$.name) {
+      const property = isValidServerProperty(props.$.name);
+      if (!property) {
+        logger.warn(
+          `${serverName} - The property ${props.$.name} does not exist!`
+        );
+      } else {
+        properties[property.name] = props.$.type || property.default;
+      }
+    }
+  }
+
+  // Fill undefined properties
+  for (const p of Object.values(ServerPropertyType)) {
+    if (!properties[p.name]) {
+      properties[p.name] = p.default;
+    }
+  }
+
+  return properties;
+}
+
+/**
+ * Check if the property exists
+ *
+ * @param {string} property property to check
+ * @returns {string} ServerProperty if property exists, undefined otherwise
+ */
+function isValidServerProperty(property) {
+  for (const p of Object.values(ServerPropertyType)) {
+    if (p.name.toLowerCase() === property.toLowerCase()) {
+      return p;
+    }
+  }
+}
+
+/**
+ *
+ * @param {object} field
+ * @param {string} message
+ */
+function assertFieldDefined(field, message) {
+  if (!field) {
+    throw new ConfigurationException(message);
+  }
+
+  return field;
+}
+
+/**
+ * @typedef Configuration
+ * @type {object}
+ * @property {string} mountDir - Mounting directory for containers.
+ * @property {PortRange} portRange - Port range to register containers.
+ * @property {{Object.<string, ServerType>}} servers - All server types avaliable.
+ */
+
+/**
+ * @typedef PortRange
+ * @type {object}
+ * @property {number} start - Port range start
+ * @property {number} end - Port range end
+ */
+
+/**
+ * @typedef ServerType
+ * @type {object}
+ * @property {string} name - Server type name
+ * @property {ContainerImage} image - Server container image
+ * @property {ServerProperties} properties - Server properties
+ */
+
+/**
+ * @typedef ServerProperties
+ * @type {object}
+ * @property {boolean} mount - Mount server container to local folder
+ * @property {boolean} autoRestart - Restart if container stopps
+ * @property {boolean} singleInstance - Keep only one instance of this server type running
+ * @property {boolean} deleteOnStop - Delete container on stop
+ */
+
+/**
+ * @typedef ContainerImage
+ * @type {object}
+ * @property {string} name - Image name
+ */
---
+++ daemon/src/server/ServerPropertyType.js
@@ -0,0 +1,18 @@
+module.exports = Object.freeze({
+  MOUNT: {
+    name: 'mount',
+    default: false,
+  },
+  AUTO_RESTART: {
+    name: 'autoRestart',
+    default: false,
+  },
+  SINGLE_INSTANCE: {
+    name: 'singleInstance',
+    default: false,
+  },
+  DELETE_ON_STOP: {
+    name: 'deleteOnStop',
+    default: true,
+  },
+});
