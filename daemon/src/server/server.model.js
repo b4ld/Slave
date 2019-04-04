32494286904fc679f02953cf31108bbc4f28f079
Heroslender <bruno.martins1998@hotmail.com>
2019-04-04 22:49:53 +0100

    Fix variable name

--- daemon/src/server/server.model.js
+++ daemon/src/server/server.model.js
@@ -25,7 +25,7 @@ module.exports = class ServerModel {
      * 
      * @type {ServerProperties}
     */
-    this.serverProperties = serverProperties;
+    this.properties = serverProperties;
   }
 };
 
