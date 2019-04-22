29f87c63cdc9d0b6c57c81748b1dac353fe863be
Heroslender <bruno.martins1998@hotmail.com>
2019-04-22 20:58:41 +0100

    Remove unneeded checks and rename vsc workspace

--- daemon/src/configuration/parsers/server.parser.ts
+++ daemon/src/configuration/parsers/server.parser.ts
@@ -1,33 +1,17 @@
-import ConfigurationException from '../exceptions/configuration.exception';
 import ServerModel from '../../server/server.model';
 import parseServerProperties from './server-property.parser';
 
-/**
- *
- * @param {T} field
- * @param {string} message
- */
-function assertFieldDefined<T>(field: T, message: string): T {
-    if (!field) {
-        throw new ConfigurationException(message);
-    }
-
-    return field;
-}
-
 export default (serverXml: any): ServerModel => {
-    assertFieldDefined(serverXml.Image, 'Server image is undefined!');
-
     if (!serverXml.Properties) {
         serverXml.Properties = { Property: [] };
     }
 
-    const name = assertFieldDefined(serverXml.Name, 'Server name is undefined!');
-    const image = {
-        name: assertFieldDefined(serverXml.Image.Name, 'Server image name is undefined!'),
-    };
-    const properties = parseServerProperties(serverXml.Name, serverXml.Properties.Property);
-
+    const name = serverXml.Name;
+    const image = serverXml.Image.Name;
+    const properties = parseServerProperties(
+        serverXml.Name,
+        serverXml.Properties.Property
+    );
     const server = new ServerModel(name, image, properties);
 
     return server;
